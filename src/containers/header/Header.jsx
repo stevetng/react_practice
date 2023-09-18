e17753993f001482b4eaf8c8cc608cdb93a75a47
Your Name <stevetng@usc.edu>
2023-09-18 12:47:50 -0400

    FINISHED HEADER

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,13 +19,15 @@ const Header = () => {
         <div className="gpt3__header-content__people">
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
+        
+        </div>
+
         </div>
         <div className='gpt3__header-image'>
           <img src={ai} alt="ai" />
-        </div>
+        </div> 
       </div>
-    </div>
   )
-}
+};
 
 export default Header
